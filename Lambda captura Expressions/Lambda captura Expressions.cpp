e3dc313ac54a07e5529835538dbaee110db5da4a
NicolasFujisawa <nicolau.fernando330@hotmail.com>
2020-04-28 01:10:14 -0300

    Capturing only variables with Lambdas.

---
+++ Lambda captura Expressions/Lambda captura Expressions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int one = 1, two = 2, three =3;
+	
+	[one, two]() { /* Capture by value */
+		cout << one << " and " << two << endl;
+	}();
+
+	[=]() { /* Capturing all local variables by value */
+		cout << one << " and " << three << endl;
+	}();
+
+	[=, &three]() { /* Capturing all by value and three by reference */
+		cout << three++ << " to " << three << endl;
+	}();
+
+	cout << three << endl;
+
+	[&]() { /* Capturing all values by reference */
+		cout << one-- << " to " << one++ << endl;
+	}();
+}
